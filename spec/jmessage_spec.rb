e6f1eb181182e39a678cc4726b1a499ec32453f9
Ricky <rickycxj@gmail.com>
2018-04-04 10:50:29 +0800

    修复rspec

--- spec/jmessage_spec.rb
+++ spec/jmessage_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Jmessage do
   it "has a version number" do
     expect(Jmessage::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
